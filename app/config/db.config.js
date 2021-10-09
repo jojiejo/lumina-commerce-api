65be575c63843c4c2766c5c5fb73ada9179da065
Djodi Ramadhan <djodi.ramadhan@primavista-solusi.com>
2021-10-09 12:40:46 +0700

    Update DB config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,13 +1,13 @@
-/*module.exports = {
-  HOST: "us-cdbr-east-04.cleardb.com",
-  USER: "b374f4301fade3",
-  PASSWORD: "fae218e1",
-  DB: "heroku_d15263974a51bb5"
-};*/
-
 module.exports = {
+  HOST: "us-cdbr-east-04.cleardb.com",
+  USER: "bfc98bf52578c4",
+  PASSWORD: "1a5ebe3d",
+  DB: "heroku_88a8286604c958d"
+};
+
+/*module.exports = {
   HOST: "localhost",
   USER: "root",
   PASSWORD: "",
   DB: "lumina"
-};
+};*/
